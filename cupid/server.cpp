e9ab966b740e9dc42edbb556221c3f9b75424738
vedantsinggh <vedantsinggh@gmail.com>
2024-02-19 23:55:19 +0530

    added close, send, receive

--- cupid/server.cpp
+++ cupid/server.cpp
@@ -1,73 +1,103 @@
-#include <iostream>
-#include <cstring>
-#include <unistd.h>
-#include <arpa/inet.h>
 #include "server.h"
-
-#define PORT 7410
 #define BUFFER_SIZE 1024
 
 void error(const char *msg) {
-    perror(msg);
-    exit(EXIT_FAILURE);
+	perror(msg);
+	exit(EXIT_FAILURE);
 }
 
-    int serverSocket, clientSocket;
-    socklen_t clientLength;
-    char buffer[BUFFER_SIZE];
-    sockaddr_in serverAddr, clientAddr;
-    int listOfClients[BUFFER_SIZE];
-    short numberOfClient = 0;
+int serverSocket;
+sockaddr_in serverAddr;
+int listOfClients[BUFFER_SIZE];
+short cindex = 0;
 
-    int cupid::createServer(){
-        serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    
-        memset((char *)&serverAddr, 0, sizeof(serverAddr));
-        serverAddr.sin_family = AF_INET;
-        serverAddr.sin_port = htons(PORT);
-        serverAddr.sin_addr.s_addr = INADDR_ANY;
+int cupid::createServer(short port){
+	serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-        if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
-            error("Error binding socket");
-        }
+	memset((char *)&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(port);
+	serverAddr.sin_addr.s_addr = INADDR_ANY;
 
-        if (listen(serverSocket, 10) == -1) {
-            error("Error listening for connections");
-        }
+	if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
+		error("Error binding socket");
+	}
 
-        std::cout << "Server is listening on port " << PORT << "...\n";
+	if (listen(serverSocket, 10) == -1) {
+		error("Error listening for connections");
+	}
 
+	std::cout << "Server is listening on port " << port << "...\n";
 
-        return serverSocket;
-    }
+	return serverSocket;
+}
 
-    int cupid::listenToClients(){
-        clientLength = sizeof(clientAddr);
-        clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr, &clientLength);
-        if (clientSocket == -1) {
-            error("Error accepting connection");
-        }
+int cupid::listenToClients(){
+	sockaddr_in clientAddr;
+	socklen_t clientLength = sizeof(clientAddr);
+	int clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr, &clientLength);
+	if (clientSocket == -1) {
+		error("Error accepting connection");
+	}	
+	listOfClients[cindex] = clientSocket;
+	cindex++;
+	std::cout<<"client connected";
+	return cindex;
+}
 
-        int bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
-        if (bytesRead == -1) {
-            error("Error receiving data");
-        }
+int cupid::closeServer(){
+	close(serverSocket);
+	return 1;
+}
 
-        std::cout << "Received message from " << clientSocket << " : " << buffer << std::endl;
+int cupid::closeClient(){
+	for (int i : listOfClients) close(i);
+	return 1;
+};
 
-        const char *response = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 5\n\nHello";
-        int bytesSent = send(clientSocket, response, strlen(response), 0);
-            std::cout << "SENT"<< std::endl;
-        if (bytesSent == -1) {
-            error("Error sending data");
-        }
+int cupid::closeClient(int index){
+	close(index);
+	return 1;
+};
 
-        // Close sockets
-        close(clientSocket);
-        return 0;
-    }
+int cupid::sendMessage(int clientIndex, std::string message){	
+	int bytesSent = send(clientIndex, message.c_str(), message.size(), 0);
+		
+	if (bytesSent == -1) {
+		error("Error sending data");
+	}
+	return bytesSent;
+};
 
-    int cupid::closeServer(){
-        close(serverSocket);
-        return 1;
-    }
+int cupid::sendMessageAll(std::string message){
+	for (int i = 0; i<cindex; i++){		
+		int bytesSent = send(listOfClients[i], message.c_str(), message.size(), 0);
+		if (bytesSent == -1) {
+			error("Error sending data");
+		}
+	}
+	return 1;
+};
+
+int cupid::receive(int index){
+	char buffer[BUFFER_SIZE];
+	int bytesRead = recv(listOfClients[index], buffer, sizeof(buffer), 0);
+	if (bytesRead == -1) {
+		error("Error receiving data");
+	}
+
+	std::cout << "Received message from " << index << " : " << buffer << std::endl;
+	return 1;
+}
+
+int cupid::receiveAll(){
+	for (int i = 0; i<cindex; i++)
+	{
+		receive(i);
+	}
+	return 1;
+}
+
+void cupid::printDetails(){
+	std::cout << cindex;
+}
--- cupid/server.h
+++ cupid/server.h
@@ -1,7 +1,8 @@
 #include <iostream>
-#include <cstring>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <string>
+#include <string.h>
 
 #define BUFFER_SIZE 1024
 
@@ -10,16 +11,28 @@ void error(const char *msg);
 class cupid{
 
 public:
-    int serverSocket, clientSocket;
-    socklen_t clientLength;
-    char buffer[BUFFER_SIZE];
-    sockaddr_in serverAddr, clientAddr;
-    int listOfClients[BUFFER_SIZE];
-    short numberOfClient;
+	int serverSocket;
+	sockaddr_in serverAddr;
+	int listOfClients[BUFFER_SIZE];
+	short cindex = 0;
 
-    int createServer();
+	int createServer(short port);
 
-    int listenToClients();
+	int listenToClients();
 
-    int closeServer();
+	int closeServer();
+		
+	int closeClient();
+
+	int closeClient(int clientIndex);
+		
+	int sendMessage(int clientIndex, std::string Message);
+
+	int sendMessageAll(std::string message);
+	
+	int receive(int index);
+
+	int receiveAll();
+
+	void printDetails();
 };
